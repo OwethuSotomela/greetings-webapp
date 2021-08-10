791805bd20f7cf73b2a260d621aee7037ef6a57e
OwethuSotomela <owethusotomela@gmail.com>
2021-08-10 10:15:38 +0200

    tests

--- test/greetings-test.js
+++ test/greetings-test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const Greeting = require("../greetings");
 const { Pool } = require("pg");
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/users';
+const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/travis_ci_test';
 
 const pool = new Pool({
     connectionString: connectionString,
